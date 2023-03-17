dcab6eee78300a8bd364d7e6a829b12571c10399
boomer77777 <max.yanchuk36@gmail.com>
2023-03-17 21:50:24 +0200

    added digit logic

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 "use strict"
 import {
     getRandomNumbers,
+    intersect,
+    switchClass,
 } from './helpers.js'
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -8,15 +10,29 @@ window.addEventListener('DOMContentLoaded', () => {
     let minesCount = 10,
         rows = 9,
         columns = 9;
-
+    
     const difficultyButtonsContainer = document.querySelector('.difficulty-level__list'),
           difficultyButtonSelector = '.difficulty-level__list__button',
           cellSelector = '.cell',
-          cellSize = 40,
-          mines = getRandomNumbers(0, rows*columns, minesCount);
+          cellSize = 40;
 
+    let mines = getRandomNumbers(0, rows*columns, minesCount);
 
-
+    const cellClasses = {
+        cell: 'cell',
+        closed: 'cell_closed',
+        clear: 'cell_clear',
+        flag: 'cell_flag',
+        d1: 'cell_digit-1',
+        d2: 'cell_digit-2',
+        d3: 'cell_digit-3',
+        d4: 'cell_digit-4',
+        d5: 'cell_digit-5',
+        d6: 'cell_digit-6',
+        d7: 'cell_digit-7',
+        d8: 'cell_digit-8',
+    };
+    
     createGame({
         rows: rows, 
         columns: columns, 
@@ -32,37 +48,23 @@ window.addEventListener('DOMContentLoaded', () => {
         const target = e.target;
 
         if (target.closest(difficultyButtonSelector)){
-        rows = target.getAttribute('data-rows');
-        columns = target.getAttribute('data-columns');
+        rows = + target.getAttribute('data-rows');
+        columns = + target.getAttribute('data-columns');
+        minesCount = + target.getAttribute('data-mines');
+        mines = getRandomNumbers(0, rows* columns, minesCount);
         }
-
-    
-        document.querySelector('.game-area').remove();
-        createGame({
-        rows: rows, 
-        columns: columns, 
-        gameAreaClass: 'game-area',
-        topPanelClass: 'game-area__top-panel',
-        resetButtonClass: 'game-area__top-panel__button',
-        timerClass: 'game-area__top-panel__timer',
-        fieldClass: 'game-area__field',
-        mineCounterClass: 'game-area__top-panel__mine-counter',
-        });
+        resetGame();
     })
 
     function createGame({rows, columns, gameAreaClass, topPanelClass, mineCounterClass, resetButtonClass, timerClass, fieldClass}) {
 
-        const gameArea = document.createElement('div');
-        const topPanel = document.createElement('div');
-        const mineCounter = document.createElement('div');
-        const resetButton = document.createElement('div');
-        const timer = document.createElement('div');
-        const field = document.createElement('div');
+        const gameArea = document.createElement('div'),
+              topPanel = document.createElement('div'),
+              mineCounter = document.createElement('div'),
+              resetButton = document.createElement('div'),
+              timer = document.createElement('div'),
+              field = document.createElement('div');
 
-        resetButton.addEventListener('click', () => {
-
-        })
-        
         const showElement = function(element, className, parentSelector){
             element.classList.add(className);
             document.querySelector(parentSelector).append(element)
@@ -74,6 +76,8 @@ window.addEventListener('DOMContentLoaded', () => {
         showElement(resetButton, resetButtonClass, '.game-area__top-panel');
         showElement(timer, timerClass, '.game-area__top-panel');
         showElement(field, fieldClass, '.game-area');
+
+        resetButton.addEventListener('click', resetGame);
 
         gameArea.style.width = cellSize * columns + "px";
         field.style.height = (cellSize * rows) + "px";
@@ -88,7 +92,8 @@ window.addEventListener('DOMContentLoaded', () => {
                 document.querySelector('.game-area__field').append(cell);
             }
         }
-        createCells('cell', 'closed');
+
+        createCells(cellClasses.cell, cellClasses.closed);
 
         document.querySelector('.game-area__field').addEventListener('click', function openCell(e) {
             const target = e.target;
@@ -100,8 +105,63 @@ window.addEventListener('DOMContentLoaded', () => {
                 cells.forEach((cell, i) => {
     
                     if (target === cell){
-                        cell.classList.add('opened')
-                        console.log(i);
+                        const checkCells = function() {
+                            let cellsToCheck = [];
+
+                            if (i === 0) {
+                                cellsToCheck = [i + 1,  i + columns, i + columns + 1]; // top left corner
+                            } else if (i === columns - 1) {
+                                cellsToCheck = [i - 1, i + columns - 1, i + columns]; // top right corner
+                            } else if (i === columns * rows - columns) {
+                                cellsToCheck = [i - columns, i - columns + 1, i + 1]; // down left corner
+                            } else if (i === columns * rows - 1) {
+                                cellsToCheck = [i - columns - 1, i - columns,  i - 1]; // down right corner
+                            } else if (i % columns === 0) {
+                                cellsToCheck = [i - columns, i - columns + 1, i + 1, i + columns, i + columns + 1]; // left side
+                            } else if (i > 0 && i < columns - 1) {
+                                cellsToCheck = [ i - 1, i + 1, i + columns - 1, i + columns, i + columns + 1]; //top side
+                            } else if (i % columns === columns - 1) {
+                                cellsToCheck = [i - columns - 1, i - columns, i - 1, i + columns - 1, i + columns]; //right side
+                            } else if (i > columns * rows - columns && i < columns * rows - 1) {
+                                cellsToCheck = [i - columns - 1, i - columns, i - columns + 1, i - 1, i + 1]; //bot side
+                            } else {
+                                cellsToCheck = [i - columns - 1, i - columns, i - columns + 1, i - 1, i + 1, i + columns - 1, i + columns, i + columns + 1];
+                            }
+
+                            const matched = intersect(mines, cellsToCheck);
+
+                            switch(matched.length) {
+                                case 0:
+                                    switchClass(cell, cellClasses.closed, cellClasses.clear);
+                                break;
+                                case 1:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d1, cellClasses.clear);
+                                break;
+                                case 2:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d2, cellClasses.clear);
+                                break;
+                                case 3:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d3, cellClasses.clear);
+                                break;
+                                case 4:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d4, cellClasses.clear);
+                                break;
+                                case 5:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d5, cellClasses.clear);
+                                break;
+                                case 6:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d6, cellClasses.clear);
+                                break;
+                                case 7:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d7, cellClasses.clear);
+                                break;
+                                case 8:
+                                    switchClass(cell, cellClasses.closed, cellClasses.d8, cellClasses.clear);
+                                break;
+                            }
+                        } 
+
+                        checkCells();
 
                         if (mines.includes(i)) {
                             cell.classList.add('mine-red')
@@ -117,6 +177,36 @@ window.addEventListener('DOMContentLoaded', () => {
                 })
             }    
         })
+
+        document.querySelector('.game-area__field').addEventListener('contextmenu', (e) => {
+            e.preventDefault();
+            const target = e.target;
+            const cells = document.querySelectorAll(cellSelector);
+            
+            if (target.closest(cellSelector )) {
+                cells.forEach((cell, i) => {
+
+                    if (target === cell){
+                        cell.classList.add(cellClasses.flag);
+                    }
+                })
+            }
+        })
+    }
+
+    function resetGame() {
+        mines = getRandomNumbers(0, rows*columns, minesCount);
+        document.querySelector('.game-area').remove();
+        createGame({
+        rows: rows, 
+        columns: columns, 
+        gameAreaClass: 'game-area',
+        topPanelClass: 'game-area__top-panel',
+        resetButtonClass: 'game-area__top-panel__button',
+        timerClass: 'game-area__top-panel__timer',
+        fieldClass: 'game-area__field',
+        mineCounterClass: 'game-area__top-panel__mine-counter',
+        });
     }
 
     
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -11,3 +11,18 @@ export function getRandomNumbers(min, max, amountOfNumbers) {
 
     return numbers
 }
+
+export function intersect(a, b){
+    let matched = [];
+    for(let element_a of a) {
+      for(let element_b of b){
+      if(element_b == element_a){matched.push(element_a)}}
+    }
+    return matched;
+};
+
+export function switchClass(elem, removeClass, addClass_a, addClass_b) {
+    elem.classList.remove(removeClass);
+    elem.classList.add(addClass_a);
+    elem.classList.add(addClass_b);
+}
